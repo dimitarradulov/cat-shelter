af871d69b44efaa07c59a947c38bfa8c8ebd16ea
Dimitar Radulov <dimitaradulov@gmail.com>
2021-10-13 13:00:19 +0300

    Added a login page

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,4 +5,14 @@ router.get('/register', (req, res) => {
   res.render('auth/register');
 });
 
+router.get('/login', (req, res) => {
+  res.render('auth/login');
+});
+
+router.post('/register', (req, res) => {
+  console.log(req.body);
+
+  res.redirect('/login');
+});
+
 module.exports = router;
