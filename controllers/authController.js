88ced10848059d0971fcaca9db7e3e50ca451b0a
Dimitar Radulov <dimitaradulov@gmail.com>
2021-10-14 12:39:37 +0300

    Added login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,16 +7,28 @@ router.get('/register', (req, res) => {
   res.render('auth/register');
 });
 
-router.get('/login', (req, res) => {
-  res.render('auth/login');
-});
-
 router.post('/register', async (req, res) => {
   const { username, password, repeatPassword } = req.body;
 
   await authService.register(username, password, repeatPassword);
 
   res.redirect('/login');
+});
+
+router.get('/login', (req, res) => {
+  res.render('auth/login');
+});
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  const auth = await authService.login(username, password);
+
+  if (auth) {
+    res.redirect('/');
+  } else {
+    res.redirect('/404');
+  }
 });
 
 module.exports = router;
--- services/authService.js
+++ services/authService.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 const register = (username, password, repeatPassword) => {
   // return bcrypt
@@ -9,8 +9,25 @@ const register = (username, password, repeatPassword) => {
   return User.create({ username, password });
 };
 
+const login = async (username, password) => {
+  try {
+    const user = await User.findOne({ username });
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (isValid) {
+      return user;
+    } else {
+      throw { message: 'Cannot find username or password' };
+    }
+  } catch {
+    return null;
+  }
+};
+
 const authService = {
   register,
+  login,
 };
 
 module.exports = authService;
